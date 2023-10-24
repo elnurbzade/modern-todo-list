a4120b0944bf663d98aa9d8fca80c742e6504d90
Elnur Bayramzada <elnurbzade@yahoo.com>
2023-10-24 14:04:53 +0400

    Js is 10% ready

---
+++ script.js
@@ -0,0 +1,6 @@
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+function addTodo() {
+    const todoText = todoInput.value.trim();
+    if (todoText === "") return;
+}
