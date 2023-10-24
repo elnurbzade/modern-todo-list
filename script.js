84391918694733190ec5b7bfb19a9b3a2ace6e91
Elnur Bayramzada <elnurbzade@yahoo.com>
2023-10-24 17:18:21 +0400

    edit is done

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
+console.log(Date.now());
+const savedTodos = JSON.parse(localStorage.getItem("todos") || []);
+for (const todo of savedTodos) {
+  addTodoList(todo);
+}
 function addTodo() {
-    const todoText = todoInput.value.trim();
-    if (todoText === "") return;
+  const todoText = todoInput.value.trim();
+  if (todoText === "") return;
+
+  const todo = {
+    id: Date.now(),
+    text: todoText,
+    completed: false,
+  };
+  savedTodos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(savedTodos));
+  addTodoToList(todo);
+  todoInput.value = "";
+}
+
+function addTodoToList(todo) {
+  const li = document.createElement("li");
+  li.setAttribute("id", todo.id);
+  li.innerHTML = `
+<span title="$(todo.text)">$(todo.text) ></span>
+<button></button> 
+<button></button> 
+<button></button> 
+`;
 }
